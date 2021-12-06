ce9ddf0d0dad842386b6835476f0a8f7720fec61
Gereon Steffens <gereon@snabble.io>
2021-12-06 14:18:22 +0100

    day 6

--- Sources/AdventOfCode/puzzle6.swift
+++ Sources/AdventOfCode/puzzle6.swift
@@ -1,22 +1,57 @@
 import Foundation
 
+// Solution for part 1: 358214
+// Solution for part 2: 1622533344325
+
 struct Puzzle6 {
-    let testData = [ "" ]
+    static let testData = [ "3,4,3,1,2" ]
 
     static func run() {
+        // let data = testData
         let data = readFile(named: "puzzle6.txt")
+
+        let initialPopulation = data[0].split(separator: ",").compactMap { Int(String($0)) }
 
         let puzzle = Puzzle6()
 
-        print("Solution for part 1: \(puzzle.part1(data))")
-        print("Solution for part 2: \(puzzle.part2(data))")
+        print("Solution for part 1: \(puzzle.part1(initialPopulation))")
+        print("Solution for part 2: \(puzzle.part2(initialPopulation))")
     }
 
-    private func part1(_ data: [String]) -> Int {
-        return 42
+    private func part1(_ initialPopulation: [Int]) -> Int {
+        let timer = Timer(day: 6); defer { timer.show() }
+        return growFish(from: initialPopulation, days: 80)
     }
 
-    private func part2(_ data: [String]) -> Int {
-        return 42
+    private func part2(_ initialPopulation: [Int]) -> Int {
+        let timer = Timer(day: 6); defer { timer.show() }
+        return growFish(from: initialPopulation, days: 256)
+    }
+
+    private func growFish(from initialPopulation: [Int], days: Int) -> Int {
+        // index: age, content: count of fish with that age
+        var today = [Int](repeating: 0, count: 9)
+        for age in initialPopulation {
+            today[age] += 1
+        }
+
+        for _ in 0..<days {
+            var tomorrow = [Int](repeating: 0, count: 9)
+
+            for (age, count) in today.enumerated() {
+                if age == 0 {
+                    tomorrow[8] += count
+                    tomorrow[6] += count
+                } else {
+                    tomorrow[age - 1] += count
+                }
+            }
+
+            today = tomorrow
+        }
+
+        return today.reduce(0) { sum, count in
+            sum + count
+        }
     }
 }
