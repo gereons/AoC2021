b5bc51884fe88760e39253aaf014e206661c0024
Gereon Steffens <gereon@steffens.org>
2021-12-09 07:58:59 +0100

    day 9b

--- Sources/AdventOfCode/puzzle9.swift
+++ Sources/AdventOfCode/puzzle9.swift
@@ -1,6 +1,39 @@
-import Foundation
+// Solution for part 1: 537
+// Solution for part 2: 1142757
 
 struct Puzzle9 {
+    struct Point: Hashable {
+        let x, y: Int
+    }
+
+    struct DepthMap {
+        let data: [[Int]]
+        let maxX: Int
+        let maxY: Int
+
+        func depthAt(_ point: Point) -> Int {
+            data[point.y][point.x]
+        }
+
+        func neighbors(for point: Point) -> [Point] {
+            var result = [Point]()
+
+            for xoffset in stride(from: -1, through: 1, by: 2) {
+                let nx = point.x+xoffset
+                if nx >= 0 && nx < maxX {
+                    result.append(Point(x: nx, y: point.y))
+                }
+            }
+            for yoffset in stride(from: -1, through: 1, by: 2) {
+                let ny = point.y+yoffset
+                if ny >= 0 && ny < maxY {
+                    result.append(Point(x: point.x, y: ny))
+                }
+            }
+            return result
+        }
+    }
+
     static let testData = [
         "2199943210",
         "3987894921",
@@ -13,56 +46,70 @@ struct Puzzle9 {
         // let data = testData
         let data = readFile(named: "puzzle9.txt")
 
-        let heightMap = Timer.time(day: 9) {
-            data.map { $0.map { Int(String($0))! } }
+        let depthMap = Timer.time(day: 9) { () -> DepthMap in
+            let map = data.map { $0.map { Int(String($0))! } }
+            return DepthMap(data: map, maxX: map[0].count, maxY: map.count)
         }
 
         let puzzle = Puzzle9()
 
-        print("Solution for part 1: \(puzzle.part1(heightMap))")
-        // print("Solution for part 2: \(puzzle.part2(data))")
+        let (riskLevel, lowPoints) = puzzle.part1(depthMap)
+        print("Solution for part 1: \(riskLevel)")
+        print("Solution for part 2: \(puzzle.part2(depthMap, lowPoints))")
     }
 
-    private func part1(_ heightMap: [[Int]]) -> Int {
-        let numRows = heightMap.count
-        let numCols = heightMap[0].count
+    private func part1(_ depthMap: DepthMap) -> (Int, [Point]) {
+        let timer = Timer(day: 9); defer { timer.show() }
         var riskLevel = 0
-        for y in 0..<heightMap.count {
-            for x in 0..<heightMap[0].count {
-                let height = heightMap[y][x]
+        var lowPoints = [Point]()
+        for y in 0..<depthMap.maxY {
+            for x in 0..<depthMap.maxX {
+                let point = Point(x: x, y: y)
+                let depth = depthMap.depthAt(point)
                 var surroundings = [Int]()
-                for neighbor in neighbors(x, y, numCols, numRows) {
-                    surroundings.append(heightMap[neighbor.1][neighbor.0])
+                for neighbor in depthMap.neighbors(for: point) {
+                    surroundings.append(depthMap.depthAt(neighbor))
                 }
                 let min = surroundings.min(by: <)!
-                if height < min {
-                    riskLevel += height + 1
+                if depth < min {
+                    lowPoints.append(point)
+                    riskLevel += depth + 1
                 }
             }
         }
 
-        return riskLevel
+        return (riskLevel, lowPoints)
     }
 
-    private func part2(_ data: [String]) -> Int {
-        return 42
+    private func part2(_ depthMap: DepthMap, _ lowPoints: [Point]) -> Int {
+        let timer = Timer(day: 9); defer { timer.show() }
+        var basinSizes = [Int]()
+        for point in lowPoints {
+            let size = findBasinSize(in: depthMap, at: point)
+            basinSizes.append(size)
+        }
+
+        return basinSizes.sorted(by: >).prefix(3).reduce(1, *)
     }
 
-    private func neighbors(_ x: Int, _ y: Int, _ maxX: Int, _ maxY: Int) -> [(Int, Int)] {
-        var result = [(Int, Int)]()
+    private func findBasinSize(in depthMap: DepthMap, at origin: Point) -> Int {
+        var visited = Set<Point>()
 
-        for xoffset in stride(from: -1, through: 1, by: 2) {
-            let nx = x+xoffset
-            if nx >= 0 && nx < maxX {
-                result.append((nx, y))
-            }
+        findBasinSize(in: depthMap, at: origin, &visited)
+        return visited.count
+    }
+
+    private func findBasinSize(in depthMap: DepthMap, at origin: Point, _ visited: inout Set<Point>) {
+        if visited.contains(origin) {
+            return
         }
-        for yoffset in stride(from: -1, through: 1, by: 2) {
-            let ny = y+yoffset
-            if ny >= 0 && ny < maxY {
-                result.append((x, ny))
+
+        visited.insert(origin)
+        for neighbor in depthMap.neighbors(for: origin) {
+            if depthMap.depthAt(neighbor) == 9 {
+                continue
             }
+            findBasinSize(in: depthMap, at: neighbor, &visited)
         }
-        return result
     }
 }
