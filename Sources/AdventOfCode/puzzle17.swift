8be4dbecf73e1eea3f5d6ecadf43012dd9c3bc8f
Gereon Steffens <gereon@steffens.org>
2021-12-17 19:37:15 +0100

    simplify day17

--- Sources/AdventOfCode/puzzle17.swift
+++ Sources/AdventOfCode/puzzle17.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Puzzle17 {
 
-    struct Velocity: Hashable {
+    struct Velocity {
         let dx: Int
         let dy: Int
     }
@@ -10,10 +10,6 @@ struct Puzzle17 {
     struct Target {
         let xRange: ClosedRange<Int>
         let yRange: ClosedRange<Int>
-
-        func hit(_ x: Int, _ y: Int) -> Bool {
-            xRange ~= x && yRange ~= y
-        }
     }
 
     static let testTarget = Target(xRange: 20 ... 30, yRange: -10 ... -5) // [ "target area: x=20..30, y=-10..-5" ]
@@ -32,36 +28,35 @@ struct Puzzle17 {
     private func part1and2(_ target: Target) -> (Int, Int) {
         let timer = Timer(day: 16); defer { timer.show() }
 
-        var heights = [Int]()
-        var velocities = Set<Velocity>()
+        var maxApex = 0
+        var velocities = 0
         for dx in 1...target.xRange.upperBound {
             for dy in target.yRange.lowerBound ... -target.yRange.lowerBound {
-                if let height = launchProbe(dx: dx, dy: dy, at: target) {
-                    heights.append(height)
-                    velocities.insert(Velocity(dx: dx, dy: dy))
+                if let apex = launchProbe(dx: dx, dy: dy, at: target) {
+                    maxApex = max(maxApex, apex)
+                    velocities += 1
                 }
             }
         }
 
-        return (heights.max()!, velocities.count)
+        return (maxApex, velocities)
     }
 
+    // if target hit, return apex, else nil
     private func launchProbe(dx: Int, dy: Int, at target: Target) -> Int? {
         var x = 0
         var y = 0
         var dx = dx
         var dy = dy
-        var apex = 0
 
         while x < target.xRange.upperBound && y > target.yRange.lowerBound {
             x += dx
             y += dy
-            apex = max(apex, y)
-            if dx != 0 { dx += (dx > 0) ? -1 : 1 }
+            if dx > 0 { dx -= 1 }
             dy -= 1
 
-            if target.hit(x, y) {
-                return apex
+            if target.xRange ~= x && target.yRange ~= y {
+                return dy * (dy + 1) / 2
             }
         }
         return nil
